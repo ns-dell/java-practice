7da2d287916c5bda2aab6f7d1c4de0f90dc96ec3
ns-dell <navdeep.sood@dell.com>
2022-08-19 14:34:40 -0500

    CompletableFuture with exceptions and HttpClient example.
    Signed-off-by: ns-dell <navdeep.sood@dell.com>

--- concurrency/src/completablefuture/ExceptionExample.java
+++ concurrency/src/completablefuture/ExceptionExample.java
@@ -1,25 +1,27 @@
 package completablefuture;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-public class AsyncExample {
+public class ExceptionExample {
 
     public static void main(String[] args){
 
-        ExecutorService executor1 = Executors.newSingleThreadExecutor();
+//        ExecutorService executor1 = Executors.newSingleThreadExecutor();
 
         Supplier<List<Long>> supplyIDs = () -> {
             sleep(200);
-            return Arrays.asList(1L, 2L, 3L);
+            throw new IllegalStateException("No data");
+//            return Arrays.asList(1L, 2L, 3L);
+        };
+
+        Function<List<Long>, List<User>> fetchUsers = ids -> {
+            sleep(300);
+            return ids.stream().map(User::new).collect(Collectors.toList());
         };
 
         Function<List<Long>, CompletableFuture<List<User>>> fetchUsers1 = ids -> {
@@ -57,15 +59,30 @@ public class AsyncExample {
             users.forEach(System.out::println);
         };
 
-        CompletableFuture<List<Long>> completableFuture = CompletableFuture.supplyAsync(supplyIDs);
-        CompletableFuture<List<User>> userFuture = completableFuture.thenCompose(fetchUsers1);
-        CompletableFuture<List<Email>> emailFuture = completableFuture.thenCompose(fetchEmails);
+//        CompletableFuture<List<Long>> completableFuture = CompletableFuture.supplyAsync(supplyIDs);
+//        CompletableFuture<List<User>> userFuture = completableFuture.thenCompose(fetchUsers1);
+//        CompletableFuture<List<Email>> emailFuture = completableFuture.thenCompose(fetchEmails);
+//
+//        CompletableFuture<List<User>> users1 = completableFuture.thenComposeAsync(fetchUsers1);
+//        CompletableFuture<List<User>> users2 = completableFuture.thenComposeAsync(fetchUsers2);
 
-        CompletableFuture<List<User>> users1 = completableFuture.thenComposeAsync(fetchUsers1);
-        CompletableFuture<List<User>> users2 = completableFuture.thenComposeAsync(fetchUsers2);
+        CompletableFuture<List<Long>> supply = CompletableFuture.supplyAsync(supplyIDs);
+        CompletableFuture<List<User>> fetch = supply.thenApply(fetchUsers);
+        CompletableFuture<Void> display = fetch.thenAccept(displayer);
 
-        sleep(6000);
-        executor1.shutdown();
+
+//        users1.thenRun(() -> System.out.println("Users 1"));
+//        users2.thenRun(() -> System.out.println("Users 2"));
+//        users1.acceptEither(users2, displayer);
+
+        sleep(1000);
+        System.out.println("Supply   : done=" + supply.isDone() +
+                            " exception=" + supply.isCompletedExceptionally());
+        System.out.println("Fetch    : done=" + supply.isDone() +
+                            " exception=" + supply.isCompletedExceptionally());
+        System.out.println("Display  : done=" + supply.isDone() +
+                            " exception=" + supply.isCompletedExceptionally());
+//        executor1.shutdown();
 
     }
 
